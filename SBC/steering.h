f5d3cd797bb47a76849fd626a5ac94c0bc26f75d
FirstKlaas <klaas.nebuhr@gmail.com>
2018-11-21 21:29:55 +0100

    typedef UpdateHandler

--- SBC/steering.h
+++ SBC/steering.h
@@ -4,6 +4,10 @@
 #include "Arduino.h"
 #include "FastLED.h"
 
+class SteeringWheel;
+
+typedef void (*UpdateHandler) (SteeringWheel &t);
+ 
 class SteeringWheel {
   private:
     uint8_t hue;
@@ -19,7 +23,7 @@ class SteeringWheel {
     void fill(const struct CRGB &color);
     void setPixelAt(const uint8_t index, const struct CRGB &color);
     void fadeOut(uint8_t hue, uint8_t startIndex);
-      
 };
+
 
 #endif
